e6434271ed19b905247421003eb221ec0d426782
Youri Sio <yourisio@gmail.com>
2020-12-23 16:52:12 +0100

    Improve level generation

--- unity/Assets/Scripts/LinesIntersection/Controller/LinesController.cs
+++ unity/Assets/Scripts/LinesIntersection/Controller/LinesController.cs
@@ -9,6 +9,7 @@ public class LinesController : MonoBehaviour {
 	[SerializeField] private GameObject shotLinePrefab;
 	[SerializeField] private Transform shotObjects;
 	[SerializeField] private Transform wallObjects;
+	[SerializeField] private int maxWalls;
 
 	private Vector3 shotStart;
 	private Vector3 shotEnd;
@@ -16,6 +17,11 @@ public class LinesController : MonoBehaviour {
 	private List<LineObject> shots = new List<LineObject>();
 
     private List<LineObject> walls = new List<LineObject>();
+
+	float MIN_WIDTH = Screen.width * 0.1f;
+	float MAX_WIDTH = Screen.width * 0.9f;
+	float MIN_HEIGHT = Screen.height * 0.1f;
+	float MAX_HEIGHT = Screen.height * 0.9f;
 
 	struct LineObject {
 		public LineSegment line;
@@ -28,37 +34,9 @@ public class LinesController : MonoBehaviour {
     }
 
     // Use this for initialization
-    void Start()
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            Vector3 screenPosition1 = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), 0));
-            Vector3 screenPosition2 = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), 0));
-            screenPosition1.z = 0;
-            screenPosition2.z = 0;
-            Vector2 projectedPosition1 = new Vector2(screenPosition1.x, screenPosition1.y);
-            Vector2 projectedPosition2 = new Vector2(screenPosition2.x, screenPosition2.y);
-
-            LineSegment newLine = new LineSegment(projectedPosition1, projectedPosition2);
-
-			// TODO: later use intersection algorithm for this
-			bool intersecting = false;
-			foreach (LineObject wall in walls) {
-				if (wall.line.Intersect(newLine) != null) {
-					intersecting = true;
-					break;
-                }
-			}
-
-			if (!intersecting) {
-				GameObject newWall = Instantiate(wallPrefab, wallObjects);
-				newWall.GetComponent<LineRenderer>().SetPosition(0, screenPosition1);
-				newWall.GetComponent<LineRenderer>().SetPosition(1, screenPosition2);
-
-				walls.Add(new LineObject(newLine, newWall));
-			}
-        }
-    }
+    void Start() {
+		GenerateNewLevel(maxWalls);
+	}
 
     // Update is called once per frame
     void Update () {
@@ -70,6 +48,9 @@ public class LinesController : MonoBehaviour {
 			AddNewShot();
         } else if (Input.GetMouseButtonDown(1)) {
 			
+        }
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			GenerateNewLevel(maxWalls);
         }
 	}
 
@@ -95,6 +76,47 @@ public class LinesController : MonoBehaviour {
         }
 		shot = null;
     }
+
+	public void GenerateNewLevel(int maxWalls) {
+		foreach (LineObject shot in shots) {
+			Destroy(shot.obj);
+        }
+		shots = new List<LineObject>();
+		foreach (LineObject wall in walls) {
+			Destroy(wall.obj);
+		}
+		walls = new List<LineObject>();
+
+		for (int i = 0; i < maxWalls; i++) {
+			Vector3 screenPosition1 = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(MIN_WIDTH, MAX_WIDTH), Random.Range(MIN_HEIGHT, MAX_HEIGHT), 0));
+			Vector3 screenPosition2 = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(MIN_WIDTH, MAX_WIDTH), Random.Range(MIN_HEIGHT, MAX_HEIGHT), 0));
+			screenPosition1.z = 0;
+			screenPosition2.z = 0;
+			Vector2 projectedPosition1 = new Vector2(screenPosition1.x, screenPosition1.y);
+			Vector2 projectedPosition2 = new Vector2(screenPosition2.x, screenPosition2.y);
+
+			LineSegment newLine = new LineSegment(projectedPosition1, projectedPosition2);
+
+			// TODO: later use sweep line for intersection check?
+			bool valid = Vector2.Distance(newLine.Point1, newLine.Point2) >= 1;
+			foreach (LineObject wall in walls) {
+				if (newLine.Intersect(wall.line) != null || newLine.DistanceToPoint(wall.line.Point1) < 1 || newLine.DistanceToPoint(wall.line.Point2) < 1) {
+					valid = false;
+					break;
+				}
+			}
+
+			if (valid) {
+				GameObject newWall = Instantiate(wallPrefab, wallObjects);
+				newWall.transform.Find("StartCastle").position = screenPosition1;
+				newWall.transform.Find("EndCastle").position = screenPosition2;
+				newWall.GetComponent<LineRenderer>().SetPosition(0, screenPosition1);
+				newWall.GetComponent<LineRenderer>().SetPosition(1, screenPosition2);
+
+				walls.Add(new LineObject(newLine, newWall));
+			}
+		}
+	}
 
 	public void RemoveShot(Vector3 pos) {
 
