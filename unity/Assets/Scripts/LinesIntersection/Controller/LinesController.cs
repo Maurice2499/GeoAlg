74b9bc04ce82764305f1468a737502fa9909e52b
SeanMcCarren <mccarrensean@gmail.com>
2020-12-23 15:27:06 +0100

    Smerige initialization

--- unity/Assets/Scripts/LinesIntersection/Controller/LinesController.cs
+++ unity/Assets/Scripts/LinesIntersection/Controller/LinesController.cs
@@ -13,8 +13,17 @@ public class LinesController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 screenPosition1 = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), Camera.main.farClipPlane / 2));
+            Vector3 screenPosition2 = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), Camera.main.farClipPlane / 2));
+            screenPosition1.z = 0;
+            screenPosition2.z = 0;
+            GameObject wall = Instantiate(wallPrefab);
+            wall.GetComponent<LineRenderer>().SetPosition(0, screenPosition1);
+            wall.GetComponent<LineRenderer>().SetPosition(1, screenPosition2);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
