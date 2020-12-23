5066c6fb11a2d0adcfc00d8bf29f46e632ab8aae
Youri Sio <yourisio@gmail.com>
2020-12-23 13:37:46 +0100

    Work in progress

--- unity/Assets/Scripts/LinesIntersection/Controller/LinesController.cs
+++ unity/Assets/Scripts/LinesIntersection/Controller/LinesController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class LinesController : MonoBehaviour {
 
+	[SerializeField] private GameObject wallPrefab;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,9 @@ public class LinesController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			GameObject wall = Instantiate(wallPrefab);
+        }
 		
 	}
 
